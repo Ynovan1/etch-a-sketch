be974775e041d2cb0fa3a1ef7a66f5e516ed37be
Ynovan <89876402+Ynovan1@users.noreply.github.com>
2025-04-17 00:11:57 -0600

    Refactor code and add new options like the black brush, rainbow brush, eraser, and Erase all as separate buttons. Add a switch button to show/hide grid. Add initial CSS design.

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let btn = document.querySelector("#btn");
+let mono = document.querySelector("#mono");
+let rainbow = document.querySelector("#rainbow");
 let container = document.querySelector("#container");
+let eraser = document.querySelector("#eraser");
+let eraseAll = document.querySelector("#eraseAll");
+let gridView = document.querySelector("#gridView");
+
+let currentSize = 16;
+let currentColorFunction = monoColorFunction;
+let isMouseDown = false;
+let lastColoredDiv = null;
+let gridVisible = false;
 
 
 function randomColor() {
@@ -8,7 +19,7 @@ function randomColor() {
         color.push(Math.floor(Math.random() * 256));
     }
     return "rgb(" + color.join(", ") + ")";
-}
+};
 
 
 function createGrid(size) {
@@ -21,26 +32,109 @@ function createGrid(size) {
         gridItem.style.height = `calc(600px / ${size})`;
         container.appendChild(gridItem);
     }
+    currentSize = size;
+    applyColorFunction();
+    updateGridVisibility();
+}
 
+function removeEventListeners() {
     let paintedDivs = document.getElementsByClassName("gridDiv");
-
     for (let i = 0; i < paintedDivs.length; i++) {
-        paintedDivs[i].addEventListener("mouseover", () => {
-            paintedDivs[i].style.backgroundColor = randomColor();
-        });
+        paintedDivs[i].removeEventListener("mousemove", colorDiv);
     }
 }
 
+function applyColorFunction() {
+    removeEventListeners();
+    let paintedDivs = document.getElementsByClassName("gridDiv");
+    if (currentColorFunction) {
+        for (let i = 0; i < paintedDivs.length; i++) {
+            paintedDivs[i].addEventListener("mousemove", colorDiv);
+        }
+    }
+}
 
-createGrid(16);
+function colorDiv(event) {
+    if (isMouseDown && event.target !== lastColoredDiv) {
+        currentColorFunction(event);
+        lastColoredDiv = event.target;
+    }
+}
 
+function monoColorFunction(event) {
+    event.target.style.backgroundColor = "rgb(0, 0, 0)";
+}
+
+function rainbowColorFunction(event) {
+    event.target.style.backgroundColor = randomColor();
+}
+
+function eraserColorFunction(event) {
+    event.target.style.backgroundColor = "white";
+}
+
+function updateGridVisibility() {
+    let gridDivs = document.getElementsByClassName("gridDiv");
+    for (let i = 0; i < gridDivs.length; i++) {
+        if (gridVisible) {
+            gridDivs[i].style.border = "solid 1px rgb(97, 97, 97)";
+        } else {
+            gridDivs[i].style.border = "none";
+        }
+    }
+    gridView.textContent = gridVisible ? "Hide grid" : "Show grid";
+}
 
 btn.addEventListener("click", () => {
     let size = parseInt(prompt("Enter the number of squares per side (1-100):"));
-
     if (size > 0 && size <= 100) {
         createGrid(size);
     } else {
         alert("Please, enter a valid number (1-100).");
     }
 });
+
+
+mono.addEventListener("click", () => {
+    currentColorFunction = monoColorFunction;
+    applyColorFunction();
+});
+
+
+rainbow.addEventListener("click", () => {
+    currentColorFunction = rainbowColorFunction;
+    applyColorFunction();
+});
+
+
+eraser.addEventListener("click", () => {
+    currentColorFunction = eraserColorFunction;
+    applyColorFunction();
+});
+
+
+eraseAll.addEventListener("click", () => {
+    let paintedDivs = document.getElementsByClassName("gridDiv");
+    for (let i = 0; i < paintedDivs.length; i++) {
+        paintedDivs[i].style.backgroundColor = "white";
+    }
+});
+
+container.addEventListener("mousedown", (event) => {
+    event.preventDefault();
+    isMouseDown = true;
+    lastColoredDiv = null;
+});
+
+container.addEventListener("mouseup", () => {
+    isMouseDown = false;
+    lastColoredDiv = null;
+});
+
+gridView.addEventListener("click", () => {
+    gridVisible = !gridVisible;
+    updateGridVisibility();
+});
+
+
+createGrid(16);
